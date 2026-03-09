96eae65ee6964a243ebadba2a5387678278128b3
Mahendra Prasad <mahendraprasath558@gmail.com>
2026-03-09 09:41:04 +0530

    week2

---
+++ Leetcode/10. Regular Expression Matching.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        
+        // dp[i][j] = whether s[0..i-1] matches p[0..j-1]
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        
+        // Base case: empty string matches empty pattern
+        dp[0][0] = true;
+        
+        // Handle patterns like "a*", "a*b*", "a*b*c*" for empty string
+        for (int j = 2; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+        
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char sc = s.charAt(i - 1);
+                char pc = p.charAt(j - 1);
+                
+                if (pc == sc || pc == '.') {
+                    // Characters match or pattern has '.'
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pc == '*') {
+                    // '*' matches zero or more of preceding element
+                    char prevChar = p.charAt(j - 2);
+                    
+                    // Option 1: Treat '*' as matching zero occurrences
+                    dp[i][j] = dp[i][j - 2];
+                    
+                    // Option 2: Treat '*' as matching one or more (if preceding char matches)
+                    if (prevChar == sc || prevChar == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
