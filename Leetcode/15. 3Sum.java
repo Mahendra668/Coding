cf7c035198feb410cfa28be5081f437226f8d5f5
Mahendra Prasad <mahendraprasath558@gmail.com>
2026-03-14 13:28:24 +0530

    Implement threeSum function to find triplets

---
+++ Leetcode/15. 3Sum.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
+        
+        // Sort the array to enable two-pointer approach
+        Arrays.sort(nums);
+        
+        // Fix one number, then use two pointers for the other two
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicate values for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            
+            // Two pointers
+            int left = i + 1;
+            int right = n - 1;
+            int target = -nums[i];
+            
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                
+                if (sum == target) {
+                    // Found a valid triplet
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    // Skip duplicates for left pointer
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    // Skip duplicates for right pointer
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    // Need larger sum, move left pointer
+                    left++;
+                } else {
+                    // Need smaller sum, move right pointer
+                    right--;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
